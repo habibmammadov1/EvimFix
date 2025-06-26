1a37d491ef2b03a71e683ee8ca0f69f3c0d58d22
Habib Mammadov <habibmammadov0@gmail.com>
2025-06-26 12:32:50 +0400

    new

--- src/main/java/com/example/evimfix/config/AppConfig.java
+++ src/main/java/com/example/evimfix/config/AppConfig.java
@@ -5,14 +5,12 @@ import java.util.TimeZone;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -22,8 +20,8 @@ import com.example.evimfix.wpAdmin.Utils.Mappers.AlqiSatqiRowMapper;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-    @Value("${file.upload-dir}")
-    private String uploadDir;
+    // @Value("${file.upload-dir}")
+    // private String uploadDir;
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
@@ -55,11 +53,11 @@ public class AppConfig implements WebMvcConfigurer {
         return validator();
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadDir);
-    }
+    // @Override
+    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    //     registry.addResourceHandler("/uploads/**")
+    //             .addResourceLocations("file:" + uploadDir);
+    // }
 
     @Bean
     public AlqiSatqiRowMapper alqisatqiRowMapper(HelperRepository helperRepository) {
